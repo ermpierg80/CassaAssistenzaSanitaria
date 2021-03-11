337f83cdbdde59f240abd9140fb46b6eec258d90
Ermanno Piergiacomi <ermanno.piergiacomi@gmail.com>
2021-03-11 09:58:16 +0100

    bozza, primo tentativo

---
+++ CassaAssistenzaSanitaria/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CassaAssistenzaSanitaria.Models;
+
+namespace CassaAssistenzaSanitaria.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
